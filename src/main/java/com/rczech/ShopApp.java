7fda30d7efe3c0bd8c4cbee5638f87ec9b318eec
RafalCzech91 <rafalczech91@gmail.com>
2018-12-16 11:23:56 +0100

    creating calass Product and Warehouse - working ver.

--- src/main/java/com/rczech/ShopApp.java
+++ src/main/java/com/rczech/ShopApp.java
@@ -1,6 +1,7 @@
 package com.rczech;
 
 import com.rczech.domain.Product;
+import com.rczech.domain.Warehouse;
 import com.rczech.hibernate.HibernateSessionRegistry;
 import org.hibernate.Session;
 
@@ -14,13 +15,38 @@ public class ShopApp {
         try {
             session.getTransaction().begin();
 
+            // save product -- start
             String name = "laptop";
             String catalogNumber = "QW132";
             Product product = new Product(name, catalogNumber);
 
             Integer productId = (Integer) session.save(product);
 
+            //save product -- end
+
             System.out.println(productId);
+
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            session.getTransaction().rollback();
+        }
+
+        try {
+            session.getTransaction().begin();
+
+            String nameW = "WarehouseOne";
+            String street = "Anders";
+            String city = "LosAngeles";
+            int postalCode = 12345;
+            int buildingNumber = 1;
+            String country = "Poland";
+
+            Warehouse warehouse = new Warehouse(nameW, street, city, postalCode, buildingNumber, country);
+
+            Integer warehouseid = (Integer) session.save(warehouse);
+
+            System.out.println(warehouseid);
 
 
             session.getTransaction().commit();
---
+++ src/main/java/com/rczech/domain/Warehouse.java
@@ -0,0 +1,39 @@
+package com.rczech.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "warehouse")
+public class Warehouse {
+
+    @Id
+    @GeneratedValue
+
+    private int id;
+
+    @Column(name = "name")
+    private String nameW;
+    private String street;
+    private String city;
+
+    @Column(name = "postal_code")
+    private int postalCode;
+
+    @Column(name = "building_number")
+    private int buildingNumber;
+    private String country;
+
+    public Warehouse(String nameW, String street, String city, int postalCode, int buildingNumber, String country) {
+
+        this.nameW = nameW;
+        this.street = street;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.buildingNumber = buildingNumber;
+        this.country = country;
+    }
+
+    private Warehouse() {
+    }
+
+}
